067d0fcddc47f1cff148a0350e9e60f2a9414301
violette <violette@air-de-violette.home>
2024-11-14 23:15:07 +0100

    added experimental features

--- darwin/Saturn/configuration.nix
+++ darwin/Saturn/configuration.nix
@@ -10,6 +10,8 @@
   services.nix-daemon.enable = true;
   nix.useDaemon = true;
   
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
+  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
