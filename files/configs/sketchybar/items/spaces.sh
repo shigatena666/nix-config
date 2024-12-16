eea897010e3d1af2319bfe096037110d9992a51d
Violette <violette@shigatena.dev>
2024-12-16 21:12:12 +0100

    added sketchybar

--- files/configs/sketchybar/items/spaces.sh
+++ files/configs/sketchybar/items/spaces.sh
@@ -7,18 +7,20 @@ sketchybar --add event aerospace_workspace_change
 SPACES=("home:" "web:󰖟" "code:" "chat:󰍩" "music:󰎆")
 
 # Add and configure spaces
-for SPACE in "${SPACES[@]}"; do
+for i in "${!SPACES[@]}"; do
+  SPACE="${SPACES[$i]}"
   WORKSPACE_NAME=${SPACE%%:*} # Extract name (everything before ':')
   ICON=${SPACE##*:}           # Extract icon (everything after ':')
+  WORKSPACE_NUMBER=$((i + 1)) # Calculate workspace number (1-based index)
 
   # Add and set space
-  sketchybar --add item "workspace.$WORKSPACE_NAME" left \
-    --subscribe "workspace.$WORKSPACE_NAME" aerospace_workspace_change \
-    --set "workspace.$WORKSPACE_NAME" \
+  sketchybar --add item "workspace.$WORKSPACE_NUMBER" left \
+    --subscribe "workspace.$WORKSPACE_NUMBER" aerospace_workspace_change \
+    --set "workspace.$WORKSPACE_NUMBER" \
     icon="$ICON" \
     label="$WORKSPACE_NAME" \
     icon.padding_right=5 \
     label.y_offset=-1 \
-    click_script="aerospace workspace $WORKSPACE_NAME" \
-    script="$PLUGIN_DIR/aerospace.sh $WORKSPACE_NAME"
+    click_script="aerospace workspace $WORKSPACE_NUMBER" \
+    script="$PLUGIN_DIR/aerospace.sh $WORKSPACE_NUMBER"
 done
